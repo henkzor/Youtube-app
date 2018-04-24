a71eb5dcb514764fdf6f8b1bd23f3beacce2ae42
Henrik <93.henrik.karlsson@gmail.com>
2018-04-24 13:48:42 +0200

    initial commit

---
+++ src/app/startsida/startsida.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startsida',
+  templateUrl: './startsida.component.html',
+  styleUrls: ['./startsida.component.css']
+})
+export class StartsidaComponent implements OnInit {
+
+	WelcomePageTitle = "YouTube-Angular";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
