c5efc47b255770600ee961dcb94ee19b6a835ab9
Henrik <93.henrik.karlsson@gmail.com>
2018-04-27 13:36:15 +0200

    h

--- src/app/videosida/videosida.component.ts
+++ src/app/videosida/videosida.component.ts
@@ -6,15 +6,57 @@ import { Http, Response } from "@angular/http";
   templateUrl: './videosida.component.html',
   styleUrls: ['./videosida.component.css']
 })
-export class VideosidaComponent implements OnInit {
+export class VideosidaComponent /*implements OnInit*/ {
 
 
-	VideoPageTitle = "Video Page";
-
+	
+	  
 	//AIzaSyBUB9Aew-KOxBbqhCFS1HYnwoEN-oGP43U
-  constructor() { }
+	/*constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
+*/
+	VideoPageTitle = "Video Page";
+	
+	
+	var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	var player;
+	function onYouTubeIframeAPIReady() 
+	{
+		player = new YT.Player('player', 
+		{
+			height: '390',
+			width: '640',
+			videoId: 'M7lc1UVf-VE',
+			events: 
+			{
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+	}  
+	   
+	  
+	function onPlayerReady(event) 
+	{
+		event.target.playVideo();
+    }  
+	
+	
+	 var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
 
 }
